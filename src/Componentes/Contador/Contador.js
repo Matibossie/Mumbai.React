1b829426a19edb5c0035dc545d04931a8ac4bf88
Matias Bossie <mati.bossie@hotmail.com>
2021-01-01 21:49:57 -0300

    clase 5

--- src/Componentes/Contador/Contador.js
+++ src/Componentes/Contador/Contador.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './contador.css'
 
 const Contador = ({cantidad, aumentarContador, resetearContador}) => {
     return (
-        <div>
-            <div className="text-center">{cantidad}</div>
+        <div className="text-center">
+            <div className="text-center contador">{cantidad}</div>
             <button className="boton next" onClick={ aumentarContador }> Agregar Item </button>
             <button className="boton next" onClick={ resetearContador }> Reset </button>
         </div>
