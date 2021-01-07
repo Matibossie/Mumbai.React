ef46bc2c2da6833e0c40d6588312be96e2e9331d
Matias Bossie <mati.bossie@hotmail.com>
2021-01-06 22:27:24 -0300

    funcionando

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React , {useState, useEffect} from 'react'
+import React , {useState} from 'react'
 import Header from './Componentes/Header'
 import Contador from './Componentes/Contador'
 import Main from './Componentes/Main'
@@ -6,50 +6,9 @@ import Catalogo from './Componentes/Catalogo'
 import Footer from './Componentes/Footer'
 
 
-const Listado = [{
-    nombre: "Frases"},{ 
-    nombre: "Pantone"},{ 
-    nombre: "Travel"},{ 
-    nombre: "Lineales"}]
+export default function App () {
 
-export default function App() {
-
-    const [categorias, setCategorias] = useState([])
-
-    useEffect(() => {
-        
-        const Categorias = new Promise((resolver, rechazar)=> {
-        setTimeout(() => {
-            // let resultado = true
-            // if(resultado){
-                resolver(Listado)
-            // }else{
-            // rechazar("Hubo un problema con las categorias")
-            //}
-        }, 2000)
-    }
-    )
-    
-    Categorias
-    .then( result => setCategorias(result))
-
-    .catch(()=> {
-        console.log("Hubo un error")
-        })
-        
-    }, [categorias])
-        
-    return (
-        <>
-            {categorias.map(product => <Catalogo nombre={product.nombre} />
-                )}
-        </>
-    )
-}
-
-
-
-/*   const [cantidad, setCantidad] = useState(0) 
+  const [cantidad, setCantidad] = useState(0) 
 
         const aumentarContador = () => {
             setCantidad(cantidad + 1)  }
@@ -70,4 +29,7 @@ return(
             resetearContador = {resetearContador}
         />
         <Footer />
-    </> */  
+    </>  
+    )
+}
+
--- src/Componentes/Catalogo.js
+++ src/Componentes/Catalogo.js
@@ -1,12 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
 
-function Catalogo ( {nombre}) {
+const Listado = [
+    {
+      nombre: "Frases"    },
+    {
+      nombre: "Pantone"    },
+    {
+      nombre: "Travel"    },
+    {
+      nombre: "Lineales"    }
+  ];
 
+
+  export default function App() {
+    const [categorias, setCategorias] = useState([]);
+  
+    useEffect(() => {
+      const Categorias = new Promise((resolver, rechazar) => {
+        setTimeout(() => {
+          // let resultado = true
+          // if(resultado){
+          resolver(Listado);
+          // }else{
+          // rechazar("Hubo un problema con las categorias")
+          //}
+        }, 2000);
+      });
+  
+      Categorias.then(result => setCategorias(result)).catch(() => {
+        console.log("Hubo un error");
+      });
+    }, [categorias]);
+  
     return (
-        <h2>{nombre}</h2>
-    )
-}
-
-export default Catalogo
-
-
+      <>
+        {categorias.map(product => (
+          <Catalogo nombre={product.nombre} />
+        ))}
+      </>
+    );
+  }
+  
+  
+  function Catalogo({ nombre }) {
+    return <h2>{nombre}</h2>;
+  }
+  
+  
