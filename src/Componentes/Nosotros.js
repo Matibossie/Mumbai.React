d20e4c48ea3503b760c6aff2e56df173c21a43f4
Matias Bossie <mati.bossie@hotmail.com>
2021-01-13 21:53:58 -0300

    clase 8

--- src/Componentes/Nosotros.js
+++ src/Componentes/Nosotros.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import '../Clases/sobreNosotros.css'
+
 
 const Nosotros = () => {
     return (
     <>
+    <main class="contenedor">
         <section class="foto-texto"> 
             <div class="foto-sobre-nosotros">
-                <img src="../img/nosotros.jpg" alt="imagen sobre nosotros"height="150px"/> 
+                <img src="https://i.ibb.co/9nrhpHx/nosotros.jpg" alt="imagen sobre nosotros"height="150px"/> 
             </div>
             <div class="contenido textos">
                 <h2>Hola! Somos Caro y Mati... </h2>
-                Yo, soy Licenciada en Publicidad y creativa incesante, por mi parte, soy Licenciado en Turismo y viajero empedernido. Y al igual que a todo el mundo, el Covid nos agarró con la guardia baja, sin herramientas, y en casa. Pero nosotros decidimos que quedarse quietos no era una alternativa. Así nació Mumbai, donde combinamos de una manera abstracta pasiones escondidas que salieron a la luz cuando menos lo esperábamos. 
-                Mumbai no es solo un proyecto casero que tomó vida y de a poco se metió en el hogar de tantas personas, en Mumbai buscamos la perfección en cada vértice, la armonía de cada línea y la combinación perfecta entre los colores que componen nuestras obras. 
-                Los invitamos a seguirnos en nuestro Instagram, darnos like en Facebook y porque no, a seguir nuestra lista de reproducción en Spotify, con la cual, nos inspiramos a la hora de diseñar, enmarcar y empaquetar cada pedido. 
-                Somos Mumbai Cuadros 
-                Dos emprendedores más, que este contexto mundial no pudo frenar. 
+                <p>&nbsp;&nbsp;&nbsp;Yo, soy Licenciada en Publicidad y creativa incesante, por mi parte, soy Licenciado en Turismo y viajero empedernido. Y al igual que a todo el mundo, el Covid nos agarró con la guardia baja, sin herramientas, y en casa. Pero nosotros decidimos que quedarse quietos no era una alternativa. Así nació Mumbai, donde combinamos de una manera abstracta pasiones escondidas que salieron a la luz cuando menos lo esperábamos. 
+                <br/><br/> 
+                &nbsp;&nbsp;&nbsp;Mumbai no es solo un proyecto casero que tomó vida y de a poco se metió en el hogar de tantas personas, en Mumbai buscamos la perfección en cada vértice, la armonía de cada línea y la combinación perfecta entre los colores que componen nuestras obras. 
+                <br/><br/>
+                &nbsp;&nbsp;&nbsp;Los invitamos a seguirnos en nuestro&nbsp;
+                <a class="text-link" target="_blank" href="https://www.instagram.com/mumbai.cuadros/" rel="noopener">Instagram</a>,
+                darnos like en&nbsp;
+                <a class="text-link" target="_blank" href="https://www.facebook.com/mumbai.cuadros" rel="noopener">Facebook</a>,
+                y porque no, a seguir nuestra lista de reproducción en&nbsp;
+                <a class="text-link" target="_blank" href="https://open.spotify.com/playlist/5odxXgNcI5NUerGLa0yGD3?si=jtO4ygPZQROUPo_aH6CYCA" rel="noopener">Spotify</a>,
+                con la cual, nos inspiramos a la hora de diseñar, enmarcar y empaquetar cada pedido. 
+                <span class="text-center"><br/><br/>Somos Mumbai Cuadros 
+                <br/><i>Dos emprendedores más, que este contexto mundial no pudo frenar. </i></span></p>
             </div>	
         </section>
         <div class="contenido textos text-center">
@@ -21,6 +32,7 @@ const Nosotros = () => {
             <iframe title="Nuestro Instagram" width="315" height="450" src="https://www.instagram.com/p/CEQA6JShgvI/embed" frameborder="0">
             </iframe>
         </div>
+    </main>
     </>
     )
 }
--- src/Componentes/SocialMedia.js
+++ src/Componentes/SocialMedia.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
 import '../Clases/SocialMedia.css'
 import Facebook from '../img/icon/facebook.svg'
 import Instagram from '../img/icon/instagram.svg'
@@ -10,7 +9,7 @@ import Spotify from '../img/icon/spotify.svg'
 function SocialMedia() {
     return (
     <>    
-           <div className="icon"><NavLink target="_blank"  rel="noreferrer" href="https://www.instagram.com/mumbai.cuadros/"><img className="icon-social" src={Instagram} alt="logo-instagram" /></NavLink></div>
+           <div className="icon"><a target="_blank"  rel="noreferrer" href="https://www.instagram.com/mumbai.cuadros/"><img className="icon-social" src={Instagram} alt="logo-instagram" /></a></div>
            <div className="icon"><a target="_blank"  rel="noreferrer" href="https://www.facebook.com/mumbai.cuadros"><img className="icon-social" src={Facebook} alt="logo-facebook" /></a></div>
            <div className="icon"><a target="_blank"  rel="noreferrer" href="https://wa.link/1d4ggt"><img className="icon-social" src={Whatsapp} alt="logo-whatsapp" /></a></div>
            <div className="icon"><a target="_blank"  rel="noreferrer" href="https://open.spotify.com/playlist/5odxXgNcI5NUerGLa0yGD3?si=jtO4ygPZQROUPo_aH6CYCA"><img className="icon-social" src={Spotify} alt="logo-spotify" /></a></div>
